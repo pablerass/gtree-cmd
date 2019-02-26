e6eb7d570f4ac1d1582f8c918e4b518caef58efe
Pablo Muñoz <pablerass@gmail.com>
2019-02-26 23:40:55 +0100

    Add search function usage

--- gtree-cmd.go
+++ gtree-cmd.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+    "fmt"
     "github.com/pablerass/gtree"
 )
 
 func main() {
-    t := gtree.New(2)
-    t.Insert(1)
-    t.Insert(4)
+    t := gtree.New("b", "2")
+    t.Insert("c", "3")
+    t.Insert("d", "4")
+    t.Insert("a", "1")
+    fmt.Println(t.Search("b"))
+    t.Insert("b", "5")
+    fmt.Println(t.Search("b"))
+    fmt.Println(t.Search("e"))
     t.Print()
 }
